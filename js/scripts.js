af389132480895934f61e485ce0770256cd03aa9
krukle <christofferfareseid@gmail.com>
2022-12-29 16:52:21 +0100

    change contact form, update texts and images

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,86 @@
 /*!
-* Start Bootstrap - Grayscale v7.0.5 (https://startbootstrap.com/theme/grayscale)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
-*/
+ * Start Bootstrap - Grayscale v7.0.5 (https://startbootstrap.com/theme/grayscale)
+ * Copyright 2013-2022 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
+ */
 //
 // Scripts
-// 
+//
 
-window.addEventListener('DOMContentLoaded', event => {
+window.addEventListener('DOMContentLoaded', (event) => {
+  // Navbar shrink function
+  var navbarShrink = function () {
+    const navbarCollapsible = document.body.querySelector('#mainNav')
+    if (!navbarCollapsible) {
+      return
+    }
+    if (window.scrollY === 0) {
+      navbarCollapsible.classList.remove('navbar-shrink')
+    } else {
+      navbarCollapsible.classList.add('navbar-shrink')
+    }
+  }
 
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
+  // Shrink the navbar
+  navbarShrink()
 
-    };
+  // Shrink the navbar when page is scrolled
+  document.addEventListener('scroll', navbarShrink)
 
-    // Shrink the navbar 
-    navbarShrink();
+  // Activate Bootstrap scrollspy on the main nav element
+  const mainNav = document.body.querySelector('#mainNav')
+  if (mainNav) {
+    new bootstrap.ScrollSpy(document.body, {
+      target: '#mainNav',
+      offset: 74,
+    })
+  }
 
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
+  // Collapse responsive navbar when toggler is visible
+  const navbarToggler = document.body.querySelector('.navbar-toggler')
+  const responsiveNavItems = [].slice.call(
+    document.querySelectorAll('#navbarResponsive .nav-link')
+  )
+  responsiveNavItems.map(function (responsiveNavItem) {
+    responsiveNavItem.addEventListener('click', () => {
+      if (window.getComputedStyle(navbarToggler).display !== 'none') {
+        navbarToggler.click()
+      }
+    })
+  })
 
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 74,
-        });
-    };
+  const emailForm = document.getElementById('email-form')
+  const emailName = document.getElementById('email-name')
+  const emailTextArea = document.getElementById('email-textarea')
+  const emailButton = document.getElementById('email-button')
 
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
+  const emailFormIsValid  = () => {
+    return emailTextArea.value.length > 0 && emailName.value.length > 0
+  }
 
-});
+  emailName.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter' && !emailFormIsValid()) e.preventDefault()
+    if (e.key === 'Enter' && emailFormIsValid()) return
+  })
+
+  emailForm.addEventListener('input', () => {
+    if (emailTextArea.value.length > 0 && emailName.value.length > 0) {
+      emailButton.classList.remove('disabled')
+    } else {
+      emailButton.classList.add('disabled')
+    }
+  })
+
+  emailButton.addEventListener('click', () => {
+    window.open(
+      `mailto:info@enterprojektering.com?subject=Anbudsförfrågan&body=${emailTextArea.value}%0D%0A%0D%0AMed vänlig hälsning,%0D%0A${emailName.value}`
+    )
+  })
+
+  document.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter' && !(window.location.toString()).includes('#signup')) {
+      event.preventDefault()
+      window.location = document.getElementById('nav-item-contact').href
+    }
+  })
+})
